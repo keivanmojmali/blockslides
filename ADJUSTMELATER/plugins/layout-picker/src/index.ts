c88aad5fea7388b9bb2be2a73d736f4acb0b6773
Keivan <mojmalik@gmail.com>
2025-10-17 20:56:54 -0400

    w

--- ADJUSTMELATER/plugins/layout-picker/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { createLayoutPickerPlugin, LayoutPickerExtension } from './layoutPickerPlugin';
-export { DEFAULT_LAYOUTS } from './layoutPickerDefaults';
-export type { LayoutTemplate } from './layoutPickerDefaults';
-export type { LayoutPickerOptions } from './layoutPickerPlugin';
-
