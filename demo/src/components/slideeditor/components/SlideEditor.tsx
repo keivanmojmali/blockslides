368b6a7a35761258a33b9c3f366c5323a038372a
Keivan <mojmalik@gmail.com>
2025-10-19 00:07:59 -0400

    w

--- demo/src/components/slideeditor/components/SlideEditor.tsx
+++ demo/src/components/slideeditor/components/SlideEditor.tsx
@@ -1,5 +1,5 @@
 import { EditorContent, SlideEditor } from '@autoartifacts/react'
-import React, { useRef } from 'react'
+import React from 'react'
 
 import { useSlideEditor } from '@/components/slideeditor/hooks/useSlideEditor'
 
@@ -13,7 +13,6 @@ export const BlockEditor = ({
   onUpdate?: (content: string) => void
   content?: string | undefined | object
 }) => {
-  const menuContainerRef = useRef(null)
   const { editor } = useSlideEditor({ onUpdate, content })
 
   if (!editor) {
@@ -22,13 +21,7 @@ export const BlockEditor = ({
 
 
   return (
-    <div className="flex h-full editor-container" ref={menuContainerRef}>
-      <div className="relative flex flex-col flex-1 h-full w-full overflow-x-hidden">
-        <div className="m-3 w-100 max-w-full">
-          <EditorContent editor={editor} />
-        </div>
-      </div>
-    </div >
+    <EditorContent editor={editor} />
   )
 }
 
