0b16a3f3b2ab7ca5d4b2861aa53a4d8ec824e912
Keivan <mojmalik@gmail.com>
2025-11-20 22:47:05 -0800

    add schema json files

---
+++ packages/ai-context/src/schemas/v1/index.ts
@@ -0,0 +1,14 @@
+export { default as imageBlock } from "./imageBlock.schema.json";
+export { default as row } from "./row.schema.json";
+export { default as column } from "./column.schema.json";
+export { default as slide } from "./slide.schema.json";
+export { default as blockquote } from "./blockquote.schema.json";
+export { default as bulletList } from "./bulletList.schema.json";
+export { default as codeBlock } from "./codeBlock.schema.json";
+export { default as hardBreak } from "./hardBreak.schema.json";
+export { default as horizontalRule } from "./horizontalRule.schema.json";
+export { default as image } from "./image.schema.json";
+export { default as heading } from "./heading.schema.json";
+export { default as paragraph } from "./paragraph.schema.json";
+export { default as youtube } from "./youtube.schema.json";
+
