e80f5a9ee706cec8bbb7509864e1b4388ef00cf5
Keivan <mojmalik@gmail.com>
2026-01-21 22:44:04 -0800

    update import file

--- packages/ai-context/src/index.ts
+++ packages/ai-context/src/index.ts
@@ -1,7 +1,5 @@
 export * as contextsV1 from "./contexts/v1";
-export * as examplesV1 from "./examples/v1";
 export * as bundlesV1 from "./bundles/v1";
-export * as recipesV1 from "./recipes/v1";
 export * as schemasV1 from "./schemas/v1";
 export * as templatesV1 from "./templates/v1";
 export * from "./types/v1";
