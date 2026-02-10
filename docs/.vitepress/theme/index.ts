1955d9ea809305129ab3b6930c735d75471613e5
Keivan <mojmalik@gmail.com>
2026-02-09 21:21:20 -0800

    updating landing page

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import CopyMarkdownButton from './components/CopyMarkdownButton.vue'
+import HeroSlideEditor from './components/HeroSlideEditor.vue'
 import './custom.css'
 
 export default {
@@ -9,4 +10,8 @@ export default {
     h(DefaultTheme.Layout, null, {
       'doc-before': () => h(CopyMarkdownButton),
     }),
+  enhanceApp({ app }) {
+    // Register globally so it can be used in the hero section
+    app.component('HeroSlideEditor', HeroSlideEditor)
+  }
 }
