902bdad77ba0e0461fc87152967045bd5578d30d
Keivan <mojmalik@gmail.com>
2026-02-10 20:09:59 -0800

    transfer react prebuilts to react-prebuilts

---
+++ packages/react-prebuilts/src/index.ts
@@ -0,0 +1,9 @@
+export * from "./SlideEditor.js";
+export * from "./useSlideEditor.js";
+export * from "./menus/BubbleMenuPreset.js";
+
+// Re-export everything from @blockslides/react core
+export * from "@blockslides/react";
+
+// Re-export everything from @blockslides/core for convenience
+export * from "@blockslides/core";
