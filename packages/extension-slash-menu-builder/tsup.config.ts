0e99d4d6ec0b2b396f4a713b0723c32e6a6ecbe8
Keivan <mojmalik@gmail.com>
2026-02-12 22:51:16 -0800

    update tsup to also build the static styles

--- packages/extension-slash-menu-builder/tsup.config.ts
+++ packages/extension-slash-menu-builder/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/index.ts', 'src/styles.css'],
   tsconfig: '../../tsconfig.build.json',
   outDir: 'dist',
   dts: true,
