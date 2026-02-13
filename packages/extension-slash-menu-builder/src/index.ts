4fdab224a8ec1d9c9896912fe4b6d8c050cee05f
Keivan <mojmalik@gmail.com>
2026-02-12 22:06:43 -0800

    adding new slash menu builder

---
+++ packages/extension-slash-menu-builder/src/index.ts
@@ -0,0 +1,12 @@
+export { SlashMenuBuilder } from './slash-menu-builder.js'
+export { createSlashCommands } from './commands.js'
+export { filterItems, flattenItems, getSelectableItems } from './filter.js'
+export { SlashMenuRenderer } from './menu-renderer.js'
+
+export type {
+  SlashMenuItem,
+  SlashMenuBuilderOptions,
+  SlashCommandContext,
+  SlashCommands,
+  MenuState,
+} from './types.js'
